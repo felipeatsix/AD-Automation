f956fce514d8b76b544348befa592472c3fe1bc6
Felipe Matheus de Souza Santos <felipe-santos@outlook.com>
2018-03-01 16:37:11 -0300

    Update Set-MyAdUser.ps1

--- Set-MyAdUser.ps1
+++ Set-MyAdUser.ps1
@@ -23,15 +23,16 @@ if($attributes.ContainsKey('Password')){
 
   $Useraccount | Set-ADUser @attributes
 
-  # Example of use: 
-
-  # cd [script location]
-  # .\Set-MyAdUser.ps1 -username [username] -attributes @{key = 'key value'}
+  <# 
+     Example of use: 
+     cd [script location]
+     .\Set-MyAdUser.ps1 -username [username] -attributes @{key = 'key value'}
+     
+     Tip:
+     The 'keys' are the Set-ADUser cmdlet parameters, you can use intellisense to discover all parameters of this cmdlet. 
+     
+     Example 2: Modifying first name, last name and initials 
   
-  # Tip:
-  # The 'keys' are the Set-ADUser cmdlet parameters, you can use intellisense to discover all parameters of this cmdlet. 
-  
-  # Example 2: Modifying first name, last name and initials 
-  
-  # cd [script location]
-  # .\Set-MyAdUser.ps1 -username [username] -attributes @{GivanName = 'Tony'; Surname = 'Stark'; Initials = 'TS'}
+     cd [script location]
+     .\Set-MyAdUser.ps1 -username [username] -attributes @{GivanName = 'Tony'; Surname = 'Stark'; Initials = 'TS'}
+#>
