61cf89cebfbfd85a4db4e7c2fd929d5913f1cb75
Felipe Matheus de Souza Santos <felipe-santos@outlook.com>
2018-03-25 21:45:39 -0300

    Update Import-NewEmployees.ps1

--- Import-NewEmployees.ps1
+++ Import-NewEmployees.ps1
@@ -2,40 +2,38 @@
 
 $Employees = Import-CSV -Path 'C:\users\Administrator\Documents\Powershell Scripts\new_employees.csv'
 
-foreach($Employee in $Employees){
-    
-  try {
-    # Create the AD user accounts
-    $NewUserParams   = @{
-        'FirstName'  = $Employee.FirstName        
-        'LastName'   = $Employee.LastName
-        'Title'      = $Employee.Title        
-    }
+foreach($Employee in $Employees){    
+# Create the AD user accounts
+    try { 
+        $NewUserParams   = @{
+            'FirstName'  = $Employee.FirstName        
+            'LastName'   = $Employee.LastName
+            'Title'      = $Employee.Title        
+        }
+            if($Employee.MiddleName){
+                $NewUserParams.MiddleName = $Employee.MiddleName
+            }
+            if($Employee.Group){
+                $NewUserParams.Group = $Employee.Group
+            }
+# Grab the username created to use for Set-MyAdUser    
+        $username = NewADUser @NewUserParams
 
-    if($Employee.MiddleName){
-       $NewUserParams.MiddleName = $Employee.MiddleName
-    }
-      if($Employee.Group){
-         $NewUserParams.Group = $Employee.Group
-      }
-
-    # Grab the username created to use for Set-MyAdUser    
-      $username = NewADUser @NewUserParams
-
-    # Create the employee's AD computer account
-    NewADcomputer -computername $Employee.ComputerName
+# Create the employee's AD computer account
+        NewADcomputer -computername $Employee.ComputerName
     
-    # Set The description for the employee's computer account
-    Set-MyADcomputer -computername $Employee.ComputerName -Attributes @{
-    'Description' = "$($Employee.FirstName) $($Employee.Lastname)'s computer"
-    }
+# Set The description for the employee's computer account
+        Set-MyADcomputer -computername $Employee.ComputerName -Attributes @{
+            'Description' = "$($Employee.FirstName) $($Employee.Lastname)'s computer"
+        }
     
-    #Set the department the employee is in
-    Set-MyAdUser -username $username -attributes @{
-    'Department' = $Employee.Department
-    }
+#Set the department the employee is in
+        Set-MyAdUser -username $username -attributes @{
+            'Department' = $Employee.Department
+        }
          
- } catch{
-      Write-Error "$($_.Execption.Message) - Line Number : $($_.InvocationInfo.ScriptLineNumber)"
-      }
+    } 
+    catch{
+        Write-Error "$($_.Execption.Message) - Line Number : $($_.InvocationInfo.ScriptLineNumber)"
+    }
 } 
